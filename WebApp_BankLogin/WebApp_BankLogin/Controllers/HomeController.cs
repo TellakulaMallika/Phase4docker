f9d6434ddbd37e3046b01236f4632b2dd3ca3138
T.Mallika <tellakulamallika@gmailc.om>
2024-02-26 00:38:55 -0700

    firstcommit

---
+++ WebApp_BankLogin/WebApp_BankLogin/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp_BankLogin.Models;
+
+namespace WebApp_BankLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        List<User> userlist = new List<User>();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            userlist.Add(new Models.User { Username = "Satyavani", Password = "satyavani@123" });
+            userlist.Add(new Models.User { Username = "Sindhu", Password = "sindhu@123" });
+            userlist.Add(new Models.User { Username = "SaiGanesh", Password = "saiganesh@123" });
+        }
+
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User u)
+        {
+            var found = userlist.Find(f => ((f.Username == u.Username) && (f.Password == u.Password)));
+
+            if (found != null)
+            {
+                return RedirectToAction("Dashboard", "Home");
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Dashboard()
+        {
+
+            return View();
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp_BankLogin/WebApp_BankLogin/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp_BankLogin.Models
+{
+    public class User
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
